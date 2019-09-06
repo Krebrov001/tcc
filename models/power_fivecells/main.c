bbd1887ead006f7ade443f4368e65eb6023659a6
Kurtis Kredo II <kkredo@csuchico.edu>
2019-09-06 14:24:18 -0700

    Completed addition of power_fivecells model.

--- models/power_fivecells/main.c
+++ models/power_fivecells/main.c
@@ -1,22 +1,49 @@
+#include "utils.h"
 #include "power_fivecells.h"
+#include <string.h>
 
-#define NUM_TIME_STEPS 10
+#define TIME_STEP_SIZE (2e-6)
 
-int main() {
-  /* Initialize model */
-  power_fivecells_initialize();
+#define SIM_TIME (0.04)
 
-  for( unsigned long step = 0; step < NUM_TIME_STEPS; ++step ){
-    /* Set model inputs here */
+#define NUM_SIM_STEPS ((long)(SIM_TIME/TIME_STEP_SIZE)+1)
 
-    /* Step the model for base rate */
-    power_fivecells_step();
+#define NUM_OUTPUTS (12)
 
-    /* Get model outputs here */
-  }
+void get_outputs(double *outputs);
 
-  printf("Result comparison not yet implemented\n");
+int main(int argc, char *argv[]) {
+	struct sim_config_T config;
 
-  return 0;
+	init_config(&config);
+	parse_args(&config, argc, argv);
+
+	const char *result_names[] = {
+		"ISRCa",    // 0
+		"ISRCb",    // 1
+		"ISRCc",    // 2
+		"VSRCa",    // 3
+		"VSRCb",    // 4
+		"VSRCc",    // 5
+		"VLOADa",   // 6
+		"VLOADb",   // 7
+		"VLOADc",   // 8
+		"ILOADa",   // 9
+		"ILOADb",   // 10
+		"ILOADc"    // 11
+	};
+	config.num_steps = NUM_SIM_STEPS;
+	config.output_names = result_names;
+	config.num_outputs = NUM_OUTPUTS;
+	config.initialize_p = power_fivecells_initialize;
+	config.step_p = power_fivecells_step;
+	config.terminate_p = NULL; // None
+	config.get_outputs_p = get_outputs;
+	config.reference_filename = "../dat_files/power_fivecells.dat";
+
+	return run_sim(&config);
 }
 
+void get_outputs(double *outputs){
+	memcpy(outputs, &rtY, sizeof(double)*NUM_OUTPUTS);
+}
